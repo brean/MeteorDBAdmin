b0e984800eb27e14561a6e64678c26e3f7d11e3b
Andreas Bresser <self@andreasbresser.de>
2018-05-18 15:26:31 +0200

    saving bugfix, add json/table-tab (without functionality for now)

--- client/main.js
+++ client/main.js
@@ -1,18 +1,23 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import './libs/w2ui-1.5.rc1.min';
-import './libs/w2ui-1.5.rc1.min.css';
+import './libs/w2ui/w2ui-1.5.rc1.min';
+import './libs/w2ui/w2ui-1.5.rc1.min.css';
 
 import './main.css';
 import './main.html';
 
 
 let w2uiGrid = null;
+let w2uiJson = null;
+let collectionName = null;
+let recids = 0;
 
-function insertRecord(name) {
-  Meteor.call('insertRecord', name, function(err, data) {
+function insertRecord(db) {
+  Meteor.call('insertRecord', collectionName, function(err, data) {
     data._ids.forEach((item_id) => {
-      w2ui['grid_'+name].add({ recid: w2ui['grid_'+name].records.length + 1, _id: item_id });
+      recids++;
+      db.push({ recid: recids, _id: item_id })
+      w2ui['grid_'+collectionName].add({ recid: recids, _id: item_id });
     });
   });
 }
@@ -24,8 +29,8 @@ function databaseEntryFromRecordId(db, recid) {
   });
 }
 
-function deleteRecord(db, name) {
-  let selection = w2ui['grid_'+name].getSelection();
+function deleteRecord(db) {
+  let selection = w2ui['grid_'+collectionName].getSelection();
   if (selection.length < 1) {
     // nothing changed
     return;
@@ -35,21 +40,24 @@ function deleteRecord(db, name) {
   if (!dbItem) {
     return;
   }
-  Meteor.call('deleteRecord', name, dbItem._id, function(err, data) {
-    showCollection(name);
+  Meteor.call('deleteRecord', collectionName, dbItem._id, function(err, data) {
+    // TODO: get selected tab name instead of hardcoded 'table'
+    showCollection(collectionName, 'table');
   })
 }
 
-function saveChanges(db, name) {
+function saveChanges(db) {
   let update = [];
-  let changes = w2ui['grid_'+name].getChanges()
+  let changes = w2ui['grid_'+collectionName].getChanges()
   if (changes.length < 1) {
     // nothing changed
+    console.log('nothing changed.');
     return;
   }
   changes.forEach((change) => {
     let dbItem = databaseEntryFromRecordId(db, change.recid)
     if (!dbItem) {
+      console.log('record id '+change.recid+' not found');
       return;
     }
 
@@ -60,22 +68,23 @@ function saveChanges(db, name) {
       fields: c,
     });
   });
-  Meteor.call('updateRecord', name, update, function(err, data) {
-    showCollection(name);
+  Meteor.call('updateRecord', collectionName, update, function(err, data) {
+    // TODO: get selected tab name instead of hardcoded 'table'
+    showCollection(collectionName, 'table');
   });
 }
 
-function buttonClicked(db, name) {
+function buttonClicked(db) {
   return function(event) {
     switch (event.target) {
       case 'add':
-        insertRecord(name);
+        insertRecord(db);
         break;
       case 'save':
-        saveChanges(db, name);
+        saveChanges(db);
         break;
       case 'delete':
-        deleteRecord(db, name);
+        deleteRecord(db);
         break;
     }
   }
@@ -89,10 +98,55 @@ function recordDeSelected(event) {
   w2uiGrid.toolbar.disable('delete');
 }
 
-function showCollection(name) {
+function createGrid(db, columnNames) {
+  let columns = [];
+  columnNames.forEach((item) => {
+    let col = {
+      field: item,
+      caption: item,
+      sortable: true,
+      searchable: true,
+      size: (1.0/columnNames.length*100)+'%'
+    };
+    if (item != '_id') {
+      col.editable = { type: 'text' };
+    }
+    columns.push(col);
+  });
+
+  let grid = {
+    name: 'grid_'+collectionName,
+    show: {toolbar: true},
+    multiSelect: false,
+    columns: columns,
+    records: db,
+    toolbar: {
+      items: [
+        { id: 'delete', type: 'button', caption: 'Delete', icon: 'w2ui-icon-cross' },
+        { id: 'add', type: 'button', caption: 'Add Record', icon: 'w2ui-icon-plus' },
+        { id: 'save', type: 'button', caption: 'Save', icon: 'w2ui-icon-check' }
+      ],
+      onClick: buttonClicked(db)
+    },
+    onSelect: recordSelected,
+    onUnselect: recordDeSelected
+  }
+  w2uiGrid = $().w2grid(grid);
+  w2uiGrid.toolbar.disable('delete');
+  window.w2ui.layout.content('main', w2uiGrid);
+}
+
+function destroyGrid() {
+  if (w2uiGrid) {
+    w2uiGrid.destroy();
+  }
+  w2uiGrid = null;
+}
+
+function showCollection(name, type) {
+  collectionName = name;
   Meteor.call('collection', name, function(err, db) {
     let columnNames = [];
-    let i = 0;
     db.forEach((item) => {
       item['_id'] = item['_id'];
       Object.keys(item).forEach((key) => {
@@ -100,48 +154,39 @@ function showCollection(name) {
           columnNames.push(key);
         }
       });
-      item['recid'] = ++i;
+      item.recid = ++recids;
     });
-
-    let columns = [];
-    columnNames.forEach((item) => {
-      let col = {
-        field: item,
-        caption: item,
-        sortable: true,
-        searchable: true,
-        size: (1.0/columnNames.length*100)+'%'
-      };
-      if (item != '_id') {
-        col.editable = { type: 'text' };
-      }
-      columns.push(col);
-    });
-
-    let grid = {
-      name: 'grid_'+name,
-      show: {toolbar: true},
-      multiSelect: false,
-      columns: columns,
-      records: db,
-      toolbar: {
-        items: [
-          { id: 'delete', type: 'button', caption: 'Delete', icon: 'w2ui-icon-cross' },
-          { id: 'add', type: 'button', caption: 'Add Record', icon: 'w2ui-icon-plus' },
-          { id: 'save', type: 'button', caption: 'Save', icon: 'w2ui-icon-check' }
-        ],
-        onClick: buttonClicked(db, name)
-      },
-      onSelect: recordSelected,
-      onUnselect: recordDeSelected
+    destroyGrid();
+    destroyJSON();
+    if (type == 'table') {
+      createGrid(db, columnNames);
+    } else if (type == 'json') {
+      createJson(db, columnNames);
     }
-    if (w2uiGrid) {
-      w2uiGrid.destroy();
-    }
-    w2uiGrid = $().w2grid(grid);
-    w2uiGrid.toolbar.disable('delete');
-    window.w2ui.layout.content('main', w2uiGrid);
   });
+  w2ui.layout.showTabs('main');
+}
+
+function createJson(db, columnNames) {
+
+}
+
+function destroyJSON() {
+  if (w2uiJson) {
+
+  }
+  w2uiJson = null;
+}
+
+function tabClicked(event) {
+  switch(event.target) {
+    case 'tab_table':
+      showCollection(collectionName, 'table');
+      break;
+    case 'tab_json':
+      showCollection(collectionName, 'json');
+      break;
+  }
 }
 
 Template.body.onCreated(function helloOnCreated() {
@@ -151,18 +196,19 @@ Template.body.onCreated(function helloOnCreated() {
       name: 'layout',
       panels: [
         { type: 'left', size: 150, resizable: true },
-        { type: 'main', content: '' /*,
+        { type: 'main', content: '',
           tabs:  {
-            active: '1table',
+            active: 'tab_table',
             tabs: [
-              { id: '1table', caption: 'table' },
-              { id: '2json', caption: 'json' }
-            ]
-          }*/
+              { id: 'tab_table', caption: 'table' },
+              { id: 'tab_json', caption: 'json' }
+            ],
+            onClick: tabClicked
+          }
         }
       ]
     });
-
+    w2ui.layout.hideTabs('main');
     let collection_nodes = [];
     let db_nodes = [{
       id: 'database',
@@ -184,7 +230,8 @@ Template.body.onCreated(function helloOnCreated() {
     	img: null,
     	nodes: db_nodes,
       onClick: (event) => {
-        showCollection(event.target);
+        // TODO: get selected tab name instead of hardcoded 'table'
+        showCollection(event.target, 'table');
       }
     }));
   });
